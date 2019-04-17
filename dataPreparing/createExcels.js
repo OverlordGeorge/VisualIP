68c3d6980e09569dc2048d5be6508ddfe5bb72ae
Unknown <surikhin@rambler.ru>
2019-04-17 11:25:07 +0700

    some changes

--- dataPreparing/createExcels.js
+++ dataPreparing/createExcels.js
@@ -2,12 +2,13 @@ let xlsx = require('xlsx');
 let fs = require('fs');
 
 let sourceFile = './data/spamtest.xlsx';
-let nameMatrix =  './dataPreparing/jsons/nameMatrix.json';
+let nameMatrix = './dataPreparing/jsons/nameMatrix.json';
 let dataMatrix = './dataPreparing/jsons/dataMatrix.json';
 
 let outputNameMatrix = './dataPreparing/results/nameMatrix.xlsx';
 let outputDataMatrix = './dataPreparing/results/dataMatrix.xlsx';
 
+let book = xlsx.utils.book_new();
 
 function getDataFromExcel(file) {
     let workbook = xlsx.readFile(file);
@@ -17,8 +18,8 @@ function getDataFromExcel(file) {
 }
 
 function readJSON(path, callback) {
-    fs.readFile(path, function(err, data) {
-        if (err){
+    fs.readFile(path, function (err, data) {
+        if (err) {
             console.log("error with file");
         }
         callback(JSON.parse(data));
@@ -28,17 +29,17 @@ function readJSON(path, callback) {
 function make() {
     let excelData = getDataFromExcel(sourceFile);
     readJSON(nameMatrix, function (data) {
-            startCreating(excelData, data);
+        startCreating(excelData, data);
     })
 
 }
 
 function getValueFromExcel(column, elem) {
     let name = column.link;
-    if (elem[name]){
+    if (elem[name]) {
         let value = elem[name];
-        if (column.values){
-            if (column.values.includes(value)){
+        if (column.values) {
+            if (column.values.includes(value)) {
                 return column.values.indexOf(value);
             }
         }
@@ -47,41 +48,61 @@ function getValueFromExcel(column, elem) {
     return "";
 }
 
-function startCreating(excelData, nameMatrix){
+function startCreating(excelData, nameMatrix) {
     let nameArr = [];
-    let valuesArr = {};
-    for (let i=0;i<nameMatrix.length;i++){
+    let valuesArr = [];
+    let num = 0;
+    for (let i = 0; i < nameMatrix.length; i++) {
         let name = nameMatrix[i].name;
         let obj = {
             desc: nameMatrix[i].desc,
-            name: name
+            name: name,
+            num: i+1
         };
-        valuesArr[name] = [];
-        if (nameMatrix[i].link){
-            for (let j=0;j<excelData.length;j++){
+        valuesArr.push([]);
+        valuesArr[i].push("K"+i);
+        if (nameMatrix[i].link) {
+            for (let j = 0; j < excelData.length; j++) {
                 let value = getValueFromExcel(nameMatrix[i], excelData[j]);
-                valuesArr[name].push(value);
+                valuesArr[num].push(value);
             }
+            num++;
         }
         nameArr.push(obj);
     }
-   // createNameMatrixExcel(nameArr);
-    createDataMatrixExcel(valuesArr);
+    createNameMatrixExcel(nameArr);
+    // createDataMatrixExcel(valuesArr);
 }
 
-function createNameMatrixExcel(arr){
+function prepareNameMatrixForExcel(arr) {
+    let res = [];
+    res.length = arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        res[i] = [];
+        res[i].push(arr[i].name);
+        res[i].push(arr[i].desc);
+    }
+    return res;
+}
+
+function createNameMatrixExcel(arr) {
+    let prepArr = prepareNameMatrixForExcel(arr);
+    let ws = xlsx.utils.json_to_sheet(prepArr);
+    xlsx.utils.book_append_sheet(book,ws, "List One")
+    //var buf = XLSX.write(wb, {type:'buffer', bookType:bookType || "xlsx"});
+    let buf = xlsx.writeFile(book, outputNameMatrix);
+    return buf;
+}
+
+function createDataMatrixExcel(arr) {
     let wb = xlsx.utils.json_to_sheet(arr);
     console.log(1);
 }
 
-function createDataMatrixExcel(arr){
-    let wb = xlsx.utils.json_to_sheet(arr);
-    console.log(1);
-}
 
-//make();
+make();
 
-let arr1 = [1,2,3];
-let arr2 = [[1,2,3],["a","b","c"]];
+let arr1 = [1, 2, 3];
+/*let arr2 = [[1, 2, 3], ["a", "b", "c"]];
 let res1 = xlsx.utils.json_to_sheet(arr1);
-let res2 = xlsx.utils.json_to_sheet(arr2);
+let res2 = xlsx.utils.json_to_sheet(arr2);*/
