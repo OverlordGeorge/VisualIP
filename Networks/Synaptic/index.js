5a2c34877ae0747336bf849d499e70f8c1fc8b94
Unknown <surikhin@rambler.ru>
2019-09-03 02:47:59 +0700

    added good nginx parser

--- Networks/Synaptic/index.js
+++ Networks/Synaptic/index.js
@@ -25,6 +25,19 @@ var trainingSet = [
     },
 ];
 
-trainer.train(trainingSet);
+/*trainer.train(trainingSet);
 let res = myNetwork.activate([0, 0]);
-console.log(res);
+console.log(res);*/
+
+let SimpleReader = require('../../my_modules/excelReader/SimpleReader').SimpleReader;
+let DataConverter = require('../../my_modules/DataConverter/DataConverter').DataConverter;
+let LogParser = require('../../my_modules/LogParser/LogParser').LogParser;
+
+let excelClass = new SimpleReader();
+let dataConverter = new DataConverter();
+let nginxParser = new LogParser('$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"');
+
+
+nginxParser.parseAndPrepareLog(__dirname+'/data/access.txt', function (data) {
+    console.log(data);
+});
